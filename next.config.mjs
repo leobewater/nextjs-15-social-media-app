0adbb7252b429ed2f84ed5e4b268cdeeef3ca920
leobewater <mmbee888@gmail.com>
2024-07-19 15:38:11 -0400

    enable cache like v14

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // enable next 14 page caching
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+    },
+  },
+};
 
 export default nextConfig;
