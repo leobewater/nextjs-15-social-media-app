ec37426719aae41e7f0d6d3ff8fc441cbe7cbe0c
leobewater <mmbee888@gmail.com>
2024-07-20 15:36:02 -0400

    add method and form action as fallback

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -17,7 +17,7 @@ const SearchField = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} method="GET" action="/search">
       <div className="relative">
         <Input name="q" placeholder="Search" className="pe-10" />
         <SearchIcon className="absolute right-3 top-1/2 size-5 -translate-y-1/2 transform text-muted-foreground" />
