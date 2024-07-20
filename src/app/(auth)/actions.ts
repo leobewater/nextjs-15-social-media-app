5d29fd3cfcc448529139c160692927e6e25acdd6
leobewater <mmbee888@gmail.com>
2024-07-20 09:21:59 -0400

    remove unused imports

--- src/app/(auth)/actions.ts
+++ src/app/(auth)/actions.ts
@@ -1,12 +1,8 @@
 "use server";
 
 import { lucia, validateRequest } from "@/auth";
-import prisma from "@/lib/prisma";
-import { loginSchema, LoginValues } from "@/lib/validation";
-import { isRedirectError } from "next/dist/client/components/redirect";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import { verify } from "@node-rs/argon2";
 
 export async function logout() {
   // https://lucia-auth.com/tutorials/username-and-password/nextjs-app
