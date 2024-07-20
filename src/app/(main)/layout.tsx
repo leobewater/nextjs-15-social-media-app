88d696238ef43fe168908589fdd1303d8c581ac6
leobewater <mmbee888@gmail.com>
2024-07-20 15:12:07 -0400

    set up layout with session provider

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/app/(main)/Navbar";
 import SessionProvider from "@/app/(main)/SessionProvider";
 import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
@@ -10,5 +11,12 @@ export default async function Layout({
   const session = await validateRequest();
   if (!session.user) redirect("/login");
 
-  return <SessionProvider value={session}>{children}</SessionProvider>;
+  return (
+    <SessionProvider value={session}>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        <div className="max-w-7xl mx-auto p-5">{children}</div>
+      </div>
+    </SessionProvider>
+  );
 }
