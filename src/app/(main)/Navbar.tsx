dfe6d3d02047775f298656b7f5a3e4ee64eaefcf
leobewater <mmbee888@gmail.com>
2024-07-20 15:27:12 -0400

    add profile dropdown menu and sign out button

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -1,7 +1,17 @@
+import UserButton from '@/components/UserButton';
+import Link from "next/link";
+
 const Navbar = () => {
   return (
-    <header>Navbar</header>
-  )
-}
+    <header className="sticky top-0 z-10 bg-card shadow-sm">
+      <div className="max-w-7xl mx-auto flex items-center justify-center flex-wrap gap-5 px-5 py-3">
+        <Link href="/" className="text-2xl font-bold text-primary">
+          bugbook
+        </Link>
+        <UserButton />
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
