bfd5aa2bd66244f839e19c41019bc2af916f1b2f
leobewater <mmbee888@gmail.com>
2024-07-22 09:43:02 -0400

    add trends sidebar

---
+++ src/components/TrendsSidebar.tsx
@@ -0,0 +1,9 @@
+const TrendsSidebar = () => {
+  return (
+    <div className="sticky top-[5.25rem] hidden md:block lg:w-80 w-72 h-fit flex-none space-y-5">
+      TrendsSidebar
+    </div>
+  );
+};
+
+export default TrendsSidebar;
