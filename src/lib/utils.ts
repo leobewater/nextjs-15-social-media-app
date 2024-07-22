99a4e99ed50e8ab2c06ec1602d983ed009273b6a
leobewater <mmbee888@gmail.com>
2024-07-22 09:38:14 -0400

    add relative date function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-
+import { formatDate, formatDistanceToNowStrict } from 'date-fns'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatRelativeDate(from: Date) {
+  const currentDate = new Date();
+  if (currentDate.getTime() - from.getTime() < 24 * 60 * 60 * 1000) {
+    // within 24 hours
+    return formatDistanceToNowStrict(from, {addSuffix: true})
+  } else {
+    if (currentDate.getFullYear() === from.getFullYear()) {
+      // within the same year
+      return formatDate(from, "MMM d");
+    } else {
+      // older more than 1 year
+      return formatDate(from, "MMM d, yyyy");
+    }
+  }
 }
