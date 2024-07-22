5d3ac756cae7d5924c47fc9072136e05264b358d
leobewater <mmbee888@gmail.com>
2024-07-22 09:02:21 -0400

    add menu bars with sticky

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import MenuBar from "@/app/(main)/MenuBar";
 import Navbar from "@/app/(main)/Navbar";
 import SessionProvider from "@/app/(main)/SessionProvider";
 import { validateRequest } from "@/auth";
@@ -15,7 +16,11 @@ export default async function Layout({
     <SessionProvider value={session}>
       <div className="flex min-h-screen flex-col">
         <Navbar />
-        <div className="max-w-7xl mx-auto p-5">{children}</div>
+        <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+          <MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
+          {children}
+        </div>
+        <MenuBar className='sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden' />
       </div>
     </SessionProvider>
   );
