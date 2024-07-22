eec208871a0e6bb8c6af158074215617f02dde55
leobewater <mmbee888@gmail.com>
2024-07-22 10:06:40 -0400

    render trending topic

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-import { formatDate, formatDistanceToNowStrict } from 'date-fns'
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { formatDate, formatDistanceToNowStrict } from "date-fns";
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function formatRelativeDate(from: Date) {
   const currentDate = new Date();
   if (currentDate.getTime() - from.getTime() < 24 * 60 * 60 * 1000) {
     // within 24 hours
-    return formatDistanceToNowStrict(from, {addSuffix: true})
+    return formatDistanceToNowStrict(from, { addSuffix: true });
   } else {
     if (currentDate.getFullYear() === from.getFullYear()) {
       // within the same year
@@ -19,4 +19,11 @@ export function formatRelativeDate(from: Date) {
       return formatDate(from, "MMM d, yyyy");
     }
   }
+}
+
+export function formatNumber(n: number): string {
+  return Intl.NumberFormat("en-US", {
+    notation: "compact",
+    maximumFractionDigits: 1,
+  }).format(n);
 }
