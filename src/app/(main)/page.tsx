6e035a7fdffdaf96066b35c89b4520f7c4015f7e
leobewater <mmbee888@gmail.com>
2024-07-22 09:38:34 -0400

    create post include to own type

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,27 +1,20 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
 import Post from "@/components/posts/Post";
 import prisma from "@/lib/prisma";
+import { postDataInclude } from "@/lib/types";
 
 export default async function Home() {
   const posts = await prisma.post.findMany({
-    include: {
-      user: {
-        select: {
-          username: true,
-          displayName: true,
-          avatarUrl: true,
-        },
-      },
-    },
+    include: postDataInclude,
     orderBy: { createdAt: "desc" },
   });
 
   return (
-    <main className="w-full ">
-      <div className="w-full">
+    <main className="w-full min-w-0">
+      <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         {posts.map((post) => (
-          <Post key={post.id}  post={post} />
+          <Post key={post.id} post={post} />
         ))}
       </div>
     </main>
