447302fcf86a89424b18f1893fb8c1bfea5b8b00
leobewater <mmbee888@gmail.com>
2024-07-22 13:30:16 -0400

    add posts to page after submittede

--- src/components/posts/editor/PostEditor.tsx
+++ src/components/posts/editor/PostEditor.tsx
@@ -6,11 +6,15 @@ import Placeholder from "@tiptap/extension-placeholder";
 import { submitPost } from "@/components/posts/editor/actions";
 import UserAvatar from "@/components/UserAvatar";
 import { useSession } from "@/app/(main)/SessionProvider";
-import { Button } from '@/components/ui/button';
-import './styles.css'
+import { Button } from "@/components/ui/button";
+import "./styles.css";
+import { useSubmitPostMutation } from "@/components/posts/editor/mutations";
+import LoadingButton from '@/components/LoadingButton';
 
 export default function PostEditor() {
   const { user } = useSession();
+
+  const mutation = useSubmitPostMutation();
 
   const editor = useEditor({
     extensions: [
@@ -30,18 +34,33 @@ export default function PostEditor() {
     }) || "";
 
   async function onSubmit() {
-    await submitPost(input);
+    // await submitPost(input);
+    mutation.mutate(input, {
+      onSuccess: () => {
+        editor?.commands.clearContent();
+      },
+    });
     editor?.commands.clearContent();
   }
 
   return (
     <div className="flex flex-col gap-5 rounded-2xl bg-card p-5 shadow-sm">
       <div className="flex gap-5">
-        <UserAvatar avatarUrl={user.avatarUrl} className='hidden sm:inline' />
-        <EditorContent editor={editor} className='w-full max-h-[20rem] overflow-y-auto bg-background rounded-2xl px-5 py-3' />
+        <UserAvatar avatarUrl={user.avatarUrl} className="hidden sm:inline" />
+        <EditorContent
+          editor={editor}
+          className="w-full max-h-[20rem] overflow-y-auto bg-background rounded-2xl px-5 py-3"
+        />
       </div>
       <div className="flex justify-end">
-        <Button onClick={onSubmit} disabled={!input.trim()} className='min-w-20'>Post</Button>
+        <LoadingButton
+          onClick={onSubmit}
+          loading={mutation.isPending}
+          disabled={!input.trim()}
+          className="min-w-20"
+        >
+          Post
+        </LoadingButton>
       </div>
     </div>
   );
---
+++ src/components/posts/editor/mutations.ts
@@ -0,0 +1,59 @@
+import { submitPost } from "@/components/posts/editor/actions";
+import { useToast } from "@/components/ui/use-toast";
+import { PostsPage } from "@/lib/types";
+import {
+  InfiniteData,
+  QueryFilters,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+
+export function useSubmitPostMutation() {
+  const { toast } = useToast();
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: submitPost,
+    onSuccess: async (newPost) => {
+      const queryFilter: QueryFilters = { queryKey: ["post-feed", "for-you"] };
+      await queryClient.cancelQueries(queryFilter);
+
+      // modify multiple feeds
+      queryClient.setQueriesData<InfiniteData<PostsPage, string | null>>(
+        queryFilter,
+        (oldData) => {
+          // get the first page and add the newPost to the first position
+          const firstPage = oldData?.pages[0];
+          if (firstPage) {
+            return {
+              pageParams: oldData.pageParams,
+              pages: [
+                // rebuild first page data
+                {
+                  posts: [newPost, ...firstPage.posts],
+                  nextCursor: firstPage.nextCursor,
+                },
+                // spread all the rest of the pages except the first page
+                ...oldData.pages.slice(1),
+              ],
+            };
+          }
+        },
+      );
+
+      toast({
+        description: "Post created",
+      });
+    },
+    onError(error) {
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Failed to post. Please try again.",
+      });
+    },
+  });
+
+  return mutation;
+}
