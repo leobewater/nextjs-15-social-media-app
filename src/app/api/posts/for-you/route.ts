228b86267846d223028d8cd882fd0c74855ccc6b
leobewater <mmbee888@gmail.com>
2024-07-22 10:47:20 -0400

    calculate to last post id as cursor for infinite scroll

--- src/app/api/posts/for-you/route.ts
+++ src/app/api/posts/for-you/route.ts
@@ -1,10 +1,17 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
+import { postDataInclude, PostsPage } from "@/lib/types";
+import { pages } from "next/dist/build/templates/app-page";
+import { NextRequest } from "next/server";
 
 // endpoint to fetch posts for infinite scroll
-export async function GET() {
+export async function GET(req: NextRequest) {
   try {
+    // for now cursor is the last post id
+    const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+
+    const pageSize = 10;
+
     const { user } = await validateRequest();
     if (!user) {
       return Response.json({ error: "Unauthorized" }, { status: 401 });
@@ -13,9 +20,18 @@ export async function GET() {
     const posts = await prisma.post.findMany({
       include: postDataInclude,
       orderBy: { createdAt: "desc" },
+      take: pageSize + 1,
+      cursor: cursor ? { id: cursor } : undefined,
     });
 
-    return Response.json(posts);
+    // get the last result post.id
+    const nextCursor = posts.length > pageSize ? posts[pageSize].id : null;
+    const data: PostsPage = {
+      posts: posts.slice(0, pageSize),
+      nextCursor,
+    };
+
+    return Response.json(data);
   } catch (error) {
     console.error(error);
     return Response.json({ error: "Internal server error" }, { status: 500 });
