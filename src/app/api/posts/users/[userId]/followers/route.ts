2a7f2e4cc85ac2f5a5ab7672083e2df1001f6002
leobewater <mmbee888@gmail.com>
2024-07-23 09:07:10 -0400

    add follow and unfollow user api endpoints

--- src/app/api/posts/users/[userId]/followers/route.ts
+++ src/app/api/posts/users/[userId]/followers/route.ts
@@ -40,3 +40,60 @@ export async function GET(
     return Response.json({ error: "Internal server error" }, { status: 500 });
   }
 }
+
+export async function POST(
+  req: Request,
+  { params: { userId } }: { params: { userId: string } },
+) {
+  try {
+    const { user: loggedInUser } = await validateRequest();
+    if (!loggedInUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    // To follow user only insert when record doesn't exist before
+    await prisma.follow.upsert({
+      where: {
+        followerId_followingId: {
+          followerId: loggedInUser.id,
+          followingId: userId,
+        },
+      },
+      create: {
+        followerId: loggedInUser.id,
+        followingId: userId,
+      },
+      update: {},
+    });
+
+    return new Response();
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params: { userId } }: { params: { userId: string } },
+) {
+  try {
+    const { user: loggedInUser } = await validateRequest();
+    if (!loggedInUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    // To un-follow user
+    await prisma.follow.deleteMany({
+      where: {
+        followerId: loggedInUser.id,
+        followingId: userId,
+      },
+    });
+
+    return new Response();
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
